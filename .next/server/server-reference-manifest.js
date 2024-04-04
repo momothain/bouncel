e89356229674731afe593aeaecd6dc2300fa754c
Morgann Thain <mthain@cs.brown.edu>
2024-04-04 10:36:29 -0400

    ignore next

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Pi9CxXssoenfCstXDA0Tgu9f9BgM5nu1qOpLLAI6QTg=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vZDZUsh1ubRp06lkpBAbWYb5Y4De3pR4EXd1xB5KEmM=\"\n}"
