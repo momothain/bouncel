861484687477eb6fa6bb153ed757299a840c8322
Morgann Thain <mthain@cs.brown.edu>
2024-04-08 15:01:12 -0400

    .next wont get ignored??

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vZDZUsh1ubRp06lkpBAbWYb5Y4De3pR4EXd1xB5KEmM=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Etq9G8+ER4+VRqIdXgYYWJqXQrq2xj/yA2fY1b8+2pA=\"\n}"
